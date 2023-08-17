09bb93bce5391c892e860023ddd2427587f37ac3
Hamza <hamza.labzioui@gmail.com>
2023-08-17 23:34:41 +0200

    Create Invoice Manager GUI

--- src/com/invoiceManager/InvoicesManager.java
+++ src/com/invoiceManager/InvoicesManager.java
@@ -19,5 +19,33 @@ public class InvoicesManager extends JFrame {
     private List<Invoice> invoices; // The list that stores instances of Invoice.
     private JTextArea textArea; // A text area used to display invoice information.
     private JTextField searchField; // A text field that allows users to search for an invoice.
+    // The InvoicesManager constructor initializes the window with a title, size, and configures the application to close when the window is closed.
+    public InvoicesManager() {
+        invoices = new ArrayList<>(); // Instance of the invoices list
 
+        setTitle("Invoice Manager "); // Sets a title for the window.
+        setSize(680, 420); // Sets the dimensions (width and height).
+        setDefaultCloseOperation(EXIT_ON_CLOSE); // Closes the window.
+
+        JPanel panel = new JPanel(); // A panel to organize graphical elements.
+        panel.setLayout(new BorderLayout());
+
+        textArea = new JTextArea(10, 30); // The text area is configured with a size and made non-editable, then added to the panel within a scrollable area (JScrollPane).
+        textArea.setEditable(false);
+        panel.add(new JScrollPane(textArea), BorderLayout.CENTER);
+
+        searchField = new JTextField(15); // The text field for search
+        JButton searchButton = new JButton("Search Invoice"); // Creates a button
+        // Add action listener for search button
+        JPanel searchPanel = new JPanel(); // Creates a panel for the search form
+        searchPanel.add(searchField); // Adds the text field to the panel
+        searchPanel.add(searchButton); // Adds the search button to the panel.
+        panel.add(searchPanel, BorderLayout.NORTH); // Adds the button to the top part of the panel.
+
+        JButton addButton = new JButton("Add Invoice"); // Creates a button
+        // Add action listener for adding invoice button
+        panel.add(addButton, BorderLayout.SOUTH); // Adds the button to the bottom part of the panel.
+
+        getContentPane().add(panel); // Adds the panel to the window.
+    }
 }
--- src/com/invoiceManager/Main.java
+++ src/com/invoiceManager/Main.java
@@ -1,8 +1,15 @@
 package com.invoiceManager;
 
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.SwingUtilities;
 
-        System.out.println("Invoice Manager");
+public class Main {
+    // The main() method starts the application
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() { // InvokeLater is used to launch the graphical interface in a separate thread.
+            public void run() {
+                // A new instance of InvoicesManager is created and made visible.
+                new InvoicesManager().setVisible(true);
+            }
+        });
     }
 }
